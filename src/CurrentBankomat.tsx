729513c706a2ff8020dec15b1cc8ab46cac5c770
Anton-Pav <ant.pavlov93@yandex.ru>
2023-04-19 15:37:31 +0300

    styled componetns

---
+++ src/CurrentBankomat.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import {MoneyType} from "./App";
+import styled from "styled-components";
+
+type CurrentBankomatPropsType = {
+    money: MoneyType
+}
+export const CurrentBankomat = (props: CurrentBankomatPropsType) => {
+    return (
+        <Banknote color={props.money.banknotes==='Dollars' ? 'aquamarine' : 'steelblue'}>
+            <Name>{props.money.banknotes}</Name>
+            <Nominal>{props.money.value}</Nominal>
+        </Banknote>
+    )
+}
+const Banknote = styled.div`
+  background-color: ${props => {
+      if (props.color==='aquamarine'){
+        return 'aquamarine'
+      } else {
+          return 'steelblue'
+      }
+  }};
+  width: 400px;
+  height: 200px;
+  margin: 10px;
+`
+
+// const BanknoteGreen = styled.div`
+//   background-color: aquamarine;
+//   width: 400px;
+//   height: 200px;
+//   margin: 10px;
+// `
+// const BanknoteBlue = styled.div`
+//   background-color: #0d62e5;
+//   width: 400px;
+//   height: 200px;
+//   margin: 10px;
+// `
+const Name = styled.span`
+  display: flex;
+  justify-content: center;
+`
+const Nominal = styled.span`
+  display: flex;
+  justify-content: center;
+  margin-top: 30px;
+  font-size: 90px;
+`
+
+
+//------------------------------------------------------------------
+// import React from 'react';
+// import {MoneyType} from "./App";
+// import styled from "styled-components";
+//
+// type CurrentBankomatPropsType = {
+//     money: MoneyType
+// }
+// export const CurrentBankomat = (props: CurrentBankomatPropsType) => {
+//     return (
+//         props.money.banknotes === 'Dollars'
+//             ?
+//             <BanknoteGreen>
+//                 <Name>{props.money.banknotes}</Name>
+//                 <Nominal>{props.money.value}</Nominal>
+//             </BanknoteGreen>
+//             : <BanknoteBlue>
+//                 <Name>{props.money.banknotes}</Name>
+//                 <Nominal>{props.money.value}</Nominal>
+//             </BanknoteBlue>
+//     )
+// }
+//
+// const BanknoteGreen = styled.div`
+//   background-color: aquamarine;
+//   width: 400px;
+//   height: 200px;
+//   margin: 10px;
+// `
+// const BanknoteBlue = styled.div`
+//   background-color: #0d62e5;
+//   width: 400px;
+//   height: 200px;
+//   margin: 10px;
+// `
+// const Name = styled.span`
+//   display: flex;
+//   justify-content: center;
+// `
+// const Nominal = styled.span`
+//   display: flex;
+//   justify-content: center;
+//   margin-top: 30px;
+//   font-size: 90px;
+// `
